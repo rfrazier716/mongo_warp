fd9e8aa7f64137151d7b91ba2c756a05daaaae70
RyanFrazier <rfrazier716@gmail.com>
2021-05-22 16:21:07 -0400

    moved routes to own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+mod routes;
 
 pub fn launch_server(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new().route(
+            "/health_check",
+            web::get().to(routes::health_check::health_check),
+        )
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
